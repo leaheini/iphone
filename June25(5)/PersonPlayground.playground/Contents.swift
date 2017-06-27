aa37c2646060faa9f698afb4cd0e4eff1806f76c
leaheini <leaheini@gmail.com>
2017-06-27 19:46:10 +0300

    Add files via upload

--- June25(5)/PersonPlayground.playground/Contents.swift
+++ June25(5)/PersonPlayground.playground/Contents.swift
@@ -8,7 +8,8 @@ import UIKit
 let dict : [String:Any] = [
     "firstname":"Benny",
     "lastname":"Berkovits",
-    "birthdate":702909559.0,
+    //"middlename":"bla",
+    "birthdate":926812800.0,
     "gender":false
 ]
 
@@ -17,34 +18,43 @@ let dict : [String:Any] = [
 class Person{
     var firstname : String
     var lastname : String
-    var middlename : String?    // ? meaning not forced
+    var middlename : String?    // ? meaning can be nil if someone doesn't have middlename
     let birthdate : Date
     var gender : Bool
    
-    /*
+    
     var fullname : String{     // compoted property
         get{
-            
+            return("\(firstname) \(middlename ?? "") \(lastname)")
         }
     }
+  
+    let now = Date()
+    
     
     var age : Int{
         get{
-            
+            let calendar = Calendar.current
+            let ageComponents = calendar.dateComponents([.year], from: birthdate, to: now)
+            let age = ageComponents.year!
+            return(age)
         }
     }
     
     var canVote : Bool{
         get{
-            
+            let hebCal = Calendar(identifier: .hebrew)
+            let hebAgeComponents = hebCal.dateComponents([.year], from: birthdate, to: now)
+            let hebAge = hebAgeComponents.year!
+            return age >= 18 || hebAge >= 18 ? true : false
         }
     }
- */
+
     
     init(_ dict : [String:Any]){
-        self.firstname = dict["firstname"] as? String ?? ""   // casting cause the value any
+        self.firstname = dict["firstname"] as? String ?? ""   // casting cause the value Any, if nil put ""
         self.lastname = dict["lastname"] as? String ?? ""
-        self.middlename = dict["middlename"] as? String   // cause he optional => can be nil
+        self.middlename = dict["middlename"] as? String   // cause it optional => can be nil
         
         let timestamp = dict["birthdate"] as? TimeInterval ?? 0         //Double
         self.birthdate = Date(timeIntervalSince1970: timestamp)
@@ -59,3 +69,6 @@ p.firstname
 p.middlename
 p.gender
 p.lastname
+p.fullname
+p.age
+p.canVote
