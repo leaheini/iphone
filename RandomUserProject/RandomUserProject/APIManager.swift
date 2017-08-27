b9685cccd5c686ca68b6f8d477a2b704c5b0cbf3
leaheini <leaheini@gmail.com>
2017-08-27 16:36:00 +0300

    27.8

---
+++ RandomUserProject/RandomUserProject/APIManager.swift
@@ -0,0 +1,49 @@
+//
+//  APIManager.swift
+//  RandomUserProject
+//
+//  Created on 27/08/2017.
+//  Copyright © 2017 leah.eini. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class APIManager: NSObject {
+    static let manager = APIManager()
+    
+    private let baseURL = "https://randomuser.me/api/"
+    
+    typealias JSON = [String:Any]
+    typealias DictionaryResultCompletion = ([JSON]?, Error?) -> Void
+
+    private func sendGetRequest(completion : @escaping DictionaryResultCompletion){
+        
+        Alamofire.request(baseURL, method: .get).responseJSON { (dataRes) in
+            
+            guard let json = dataRes.result.value as? JSON else{
+                //notify about an error
+                completion(nil, dataRes.error)
+                return
+            }
+            
+            let data = json[""] as? JSON ?? [:]
+            let jsonArr = data["results"] as? [JSON] ?? []
+            
+            completion(jsonArr, nil)
+            
+        }
+    }
+    
+    //****** the page isue  with params
+    func getUsers(page : UInt,
+                  completion : @escaping ([User]?, Error?)->Void){
+        
+        sendGetRequest() { (jsonArr, err) in
+            let arr = jsonArr?.flatMap{ User($0) }
+            completion(arr, err)
+        }
+    }
+
+    
+}
---
+++ RandomUserProject/RandomUserProject/GalleryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GalleryViewController.swift
+//  RandomUserProject
+//
+//  Created on 27/08/2017.
+//  Copyright © 2017 leah.eini. All rights reserved.
+//
+
+import UIKit
+
+class GalleryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
